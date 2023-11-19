1cc9c049eeb9760c56d963b059b74bb76cb2d25d
김민석 <63552400+kms-git0205@users.noreply.github.com>
2023-11-20 00:46:32 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=python3) 
 
 ### 성능 요약
 
-메모리: 4.22 MB, 시간: 0.04 ms
+메모리: 10.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 1일 0:32:3
+2023년 11월 1일 0:46:31
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,14 +1,28 @@
 def solution(name):
     answer = 0
-    lr = len(name) - 1
-    for i, s in enumerate(name):
-        answer += min(ord(s) - ord("A"), ord("Z")-ord(s)+1)
-
-        next = i + 1
-        while next < len(name) and name[next] == 'A':
-            next += 1
-        
-        lr = min([lr, 2 * i + len(name) - next, i + 2 * (len(name) - next)])
-    answer += lr
     
-    return answer
+    move = len(name) - 1
+    
+            
+    for i in range(len(name)):
+        if ord(name[i]) <= ord('N'):
+            answer += ord(name[i]) - ord('A')
+        else:
+            answer += ord('Z') - ord(name[i]) + 1
+
+        idx = i + 1
+
+        # A가 안나올 때까지 계속 오른쪽으로 idx를 이동
+        while idx < len(name) and name[idx] == 'A':
+            idx += 1
+
+        # i: [0]에서 오른쪽으로 이동해서 [i]까지 갈 때의 이동 횟수
+        # len(name) - idx: [0]에서 왼쪽으로 이동해서 [idx]까지 갈 때의 이동 횟수
+        left_right = (len(name) - idx) * 2 + i  # [0] -> [i](오른쪽) -> [idx](왼쪽)
+        right_left = i * 2 + (len(name) - idx)  # [0] -> [idx](왼쪽) -> [i](오른쪽)
+
+        move = min(move, right_left, left_right)
+
+    answer += move  # 좌우 이동 횟수
+    print(move)
+    return answer    
